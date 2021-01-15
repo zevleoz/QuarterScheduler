bdc8f3449b79d6d908f56b804bcd777426cd4b2d
Jeff Lau <siulau2022@u.northwestern.edu>
2021-01-15 10:28:29 -0600

    Course List

--- App.js
+++ App.js
@@ -1,16 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, SafeAreaView, ScrollView} from 'react-native';
 
 const schedule = {
-  title:"CS Courses for 2020-2021"
+  title:"CS Courses for 2020-2021",
+  "courses": [
+    {
+      "id": "F101",
+      "title": "Computer Science: Concepts, Philosophy, and Connections",
+      "meets": "MWF 11:00-11:50"
+    },
+    {
+      "id": "F110",
+      "title": "Intro Programming for non-majors",
+      "meets": "MWF 10:00-10:50"
+    },
+    {
+      "id": "F111",
+      "title": "Fundamentals of Computer Programming I",
+      "meets": "MWF 13:00-13:50"
+    },
+    {
+      "id": "F211",
+      "title": "Fundamentals of Computer Programming II",
+      "meets": "TuTh 12:30-13:50"
+    }
+  ]
 }
+
+const Banner = ({title}) => (
+  <Text style = {styles.bannerStyle}> {title}</Text>
+);
+
+const CourseList = ({courses}) => (
+  <ScrollView>
+    <View style={styles.courseList}>
+      {courses.map(course => <Course key={course.id} course={course} />)}
+    </View>
+  </ScrollView>
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1)
+);
+
+const Course = ({course}) =>(
+  <TouchableOpacity style ={styles.courseButton}>
+    <Text style = {styles.courseText}>
+      {`CS ${getCourseNumber(course)}\n${course.meets}`}
+    </Text>
+  </TouchableOpacity>
+)
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.bannerStyles}>{schedule.title}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Banner title={schedule.title}/>
+      <CourseList courses={schedule.courses} />
+    </SafeAreaView>
   );
 }
 
@@ -19,11 +66,35 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
   },
-  bannerStyles: {
+  banner: {
     color: '#888',
     fontSize: 32,
-  }
+  },
+  courseList: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  courseButton: {
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    height: 60,
+    padding: 10,
+    minWidth: 90,
+    maxWidth: 90,
+    backgroundColor: '#66b0ff',
+  },
+  courseText:{
+    color: '#fff',
+    fontSize: 12,
+    textAlign: 'center',
+  },
 });
 
 export default App;
